a26aab793917a6273148028a36a12e96048e8ebe
RomioKy <32262029+RomioKy@users.noreply.github.com>
2017-10-30 15:32:01 +0000

    up

--- ExercicioFinal1/ex2/sketch.js
+++ ExercicioFinal1/ex2/sketch.js
@@ -1,5 +1,4 @@
 var img;
-var smallPoint, largePoint;
 
 function preload() {
   img = loadImage("images/eva.jpg");
@@ -7,8 +6,6 @@ function preload() {
 
 function setup() {
   createCanvas(720, 400);
-  smallPoint = 4;
-  largePoint = 40;
   imageMode(CENTER);
   noStroke();
   background(255);
@@ -16,9 +13,9 @@ function setup() {
 }
 
 function draw() {
-  var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
   var x = floor(random(img.width));
   var y = floor(random(img.height));
   var pix = img.get(x, y);
   fill(pix, 128);
-  ellipse(x, y, pointillize, pointillize);
+  ellipse(x, y, 10, 10);
+}
