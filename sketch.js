631b42d051b2ebf09148bffa2127eeac778b7091
RomioKy <32262029+RomioKy@users.noreply.github.com>
2017-11-12 19:27:40 +0000

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var xFolha,yFolha,factorCrescimento,  ponto1, ponto2;
 var cores=[];
 
-//function preload() {img = loadImage("eva.png");  //Carrega a imagem}
+function preload() {img = loadImage("eva.png");  //Carrega a imagem
+}
 
 function setup() 
 {
